4692ff40a16046ab54106643da8be668ad645738
yehitsmee <chaithra2900@gmail.com>
2025-06-25 08:33:29 +0530

    Initial Commit

---
+++ ElvisCostello/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ElvisCostello
+//
+//  Created by Chaithra Nayak on 25/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ElvisCostello/ElvisCostelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisCostelloApp.swift
+//  ElvisCostello
+//
+//  Created by Chaithra Nayak on 25/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisCostelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
