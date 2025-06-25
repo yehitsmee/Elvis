e901468bcda33d49ee9a19fe60233336ac421cae
yehitsmee <chaithra2900@gmail.com>
2025-06-25 08:58:34 +0530

    update1

--- ElvisCostello/ContentView.swift
+++ ElvisCostello/ContentView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = ""
+    @State private var showImage: String = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What's So Funny 'Bout")
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            Spacer()
+            Image(systemName: showImage)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+            Text(message)
+                .foregroundStyle(.purple)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            Spacer()
+            HStack {
+                Button("Peace") {
+                    showImage = "peacesign"
+                    message = "Peace"
+                }
+                Button("Love") {
+                    showImage = "heart"
+                    message = "Love"
+                }
+                Button("Understanding") {
+                    showImage = "lightbulb"
+                    message = "Understanding"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
         }
         .padding()
     }
